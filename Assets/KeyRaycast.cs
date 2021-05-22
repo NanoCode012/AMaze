6638496c054e7f717180fd7610f44f8522c50629
razermanth <78356885+razermanth@users.noreply.github.com>
2021-05-23 02:28:38 +0700

    mapv2updated(doorcodeerror)

---
+++ Assets/KeyRaycast.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace KeySystem
+{
+    public class KeyRaycast : MonoBehaviour
+    {
+        [SerializeField] private int rayLength = 5;
+        [SerializeField] private LayerMask layerMaskInteract;
+        [SerializeField] private string excluseLayerName = null;
+
+        private KeyItemController raycastedObject;
+        [SerializeField] private KeyCode openDoorKey = KeyCode.Mouse0;
+
+        [SerializeField] private Image Crosshair = null;
+        private bool isCrosshairActive;
+        private bool doOnce;
+
+        private string interactableTag = "InteractiveObject";
+
+        private string Update()
+        {
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection.(Vector3.forward);
+
+            int mask = 1 << LayerMask.NameToLayer(excluseLayerName) | layerMaskInteract.value;
+
+            if (Physics.Raycast(transform.position, fwd, out hit, rayLength, mask))
+            {
+                if(hit.collider.CompareTag(interactableTag))
+                {
+                    if (!doOnce)
+                    {
+                        raycastedObject = hit.collider.gameObject.GetComponent<KeyItemController>();
+                        CrosshairChange(true);
+                    }
+
+                    isCrosshairActive = true;
+                    doOnce = true;
+
+                    if (input.GetKeyDown(openDoorKey))
+                    {
+                        raycastedObject.ObjectInteraction();
+                    }
+                }
+            }
+            else;
+            {
+                if(isCrosshairActive)
+                {
+                CrosshairChange(false);
+                doOnce = false;
+                }
+            }
+        }
+
+        void CrosshairChange(bool on)
+        {
+            if (on && !doOnce)
+            {
+                Crosshair.color = Color.red;
+            }
+            else
+            {
+                Crosshair.color = Color.white;
+                isCrosshairActive = false;
+            }
+        }
+    }
+}
