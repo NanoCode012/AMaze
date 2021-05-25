17200bff821eb2bb173dd219c397dd41ad7401f3
NanoCode012 <kevinvong@rocketmail.com>
2021-05-26 00:45:47 +0700

    Fix material (#15)
    * Improve condition check
    
    * Fix color missing

--- Assets/Scripts/AutoTransparent.cs
+++ Assets/Scripts/AutoTransparent.cs
@@ -15,10 +15,11 @@ public class AutoTransparent : MonoBehaviour
     void Start()
     {
         rendererTarget = GetComponent<Renderer>();
-        if (rendererTarget == null || rendererTarget.material == null)
+        if (!rendererTarget || !rendererTarget.material || rendererTarget.material.color == null)
         {
             Debug.LogError("Missing renderer or material");
             Destroy(this);
+            return;
         }
         m_OldShader = rendererTarget.material.shader;
         m_OldColor = rendererTarget.material.color;
