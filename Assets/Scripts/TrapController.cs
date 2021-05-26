aa076cd9534bea476166848a4243b1077e51623c
NanoCode012 <kevinvong@rocketmail.com>
2021-05-27 01:02:35 +0700

    Add damage (#28)

--- Assets/Scripts/TrapController.cs
+++ Assets/Scripts/TrapController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TrapController : MonoBehaviour
 {
+    private enum TrapType
+    {
+        None,
+        Axe,
+        Spear,
+        Saw,
+        Hammer
+    }
+
     public void HandleHit(PlayerController player, GameObject obj)
     {
         if (!obj) return;
         if (obj.tag != "Trap" && obj.transform.root.tag != "Trap") return;
 
-        if (obj.GetComponent<Axe>())
+        var trapType = GetType(obj);
+
+        switch (trapType)
         {
-            print("hit by axe");
-            player.Hp -= 0.8f;
+            case TrapType.Axe:
+                print("hit by axe");
+                player.Hp -= 0.8f;
+                break;
+            case TrapType.Spear:
+                print("hit by spear");
+                player.Hp -= 0.3f;
+                break;
+            case TrapType.Saw:
+                print("hit by saw");
+                player.Hp -= 0.6f;
+                break;
+            case TrapType.Hammer:
+                print("hit by hammer");
+                player.Hp -= 0.4f;
+                break;
+            case TrapType.None:
+                break;
         }
 
         StartCoroutine(PauseObj(obj));
@@ -24,5 +51,15 @@ public class TrapController : MonoBehaviour
         obj.SetActive(false);
         yield return new WaitForSeconds(time);
         obj.SetActive(true);
+    }
+
+    private TrapType GetType(GameObject obj)
+    {
+        if (obj.GetComponent<Axe>()) return TrapType.Axe;
+        if (obj.GetComponent<Spear>()) return TrapType.Spear;
+        if (obj.GetComponent<SawBlade>()) return TrapType.Saw;
+        if (obj.GetComponent<Hammer>()) return TrapType.Hammer;
+
+        return TrapType.None;
     }
 }
