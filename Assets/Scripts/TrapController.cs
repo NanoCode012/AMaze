8da4214608d03fbf32946e15b3e80b740f8bb96b
NanoCode012 <kevinvong@rocketmail.com>
2021-05-26 17:20:35 +0700

    Add damage

---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public void HandleHit(PlayerController player, GameObject obj)
+    {
+        if (!obj) return;
+        if (obj.tag != "Trap" && obj.transform.root.tag != "Trap") return;
+
+        if (obj.GetComponent<Axe>())
+        {
+            print("hit by axe");
+            player.Hp -= 0.8f;
+        }
+
+        StartCoroutine(PauseObj(obj));
+    }
+
+    // Pause an object `obj` from doing dmg for `time` seconds
+    private IEnumerator PauseObj(GameObject obj, float time = 1f)
+    {
+        obj.SetActive(false);
+        yield return new WaitForSeconds(time);
+        obj.SetActive(true);
+    }
+}
