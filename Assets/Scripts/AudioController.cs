5b4f485fad0b644fc6c18c43a2c4ed1701336d6b
NanoCode012 <kevinvong@rocketmail.com>
2021-05-27 22:47:20 +0700

    Add AudioController

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] public class DictionaryAudioClips : SerializableDictionary<string, AudioClip> { }
+
+[System.Serializable]
+public struct Clips
+{
+    public string Name;
+    public AudioClip clip;
+}
+
+public class AudioController : MonoBehaviour
+{
+
+    public List<Clips> clips;
+
+    public void PlayClip(string clipName, Vector3 position)
+    {
+        var clip = GetClip(clipName);
+        if (clip) AudioSource.PlayClipAtPoint(clip, position);
+    }
+
+    private AudioClip GetClip(string name)
+    {
+        for (int i = 0; i < clips.Count; i++)
+            if (clips[i].Name == name) return clips[i].clip;
+
+        return null;
+    }
+
+
+}
