12edccf2d9e5440f4b702ad9e76b8aa5029f2a02
zombiedude91 <newnack2013@gmail.com>
2021-05-27 20:54:58 +0700

    button sound

---
+++ Assets/Scenes/ButtonClickSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Plugins.ButtonSoundsEditor
+{
+    public class ButtonClickSound : MonoBehaviour, IPointerClickHandler
+    {
+        public AudioSource AudioSource;
+        public AudioClip ClickSound;
+
+        public void PlayClickSound()
+        {
+            AudioSource.PlayOneShot(ClickSound);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            PlayClickSound();
+        }
+    }
+
+}
