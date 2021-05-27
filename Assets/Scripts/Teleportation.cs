41aa57de8f9a2c6a6f5d7af78ea1ba75720c5257
razermanth <78356885+razermanth@users.noreply.github.com>
2021-05-27 17:15:30 +0700

    .

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+
+    public GameObject Destination;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<CharacterController>();
+        if (player)
+        {
+            StartCoroutine(Teleport(player));
+        }
+    }
+
+    private IEnumerator Teleport(CharacterController controller)
+    {
+        yield return new WaitForSeconds(1);
+        controller.enabled = false;
+        controller.transform.position = Destination.transform.position;
+        controller.enabled = true;
+    }
+}
