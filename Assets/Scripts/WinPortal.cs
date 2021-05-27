40ca043d3c1552e4121efc15dd82072530f55269
NanoCode012 <kevinvong@rocketmail.com>
2021-05-28 03:21:06 +0700

    Add Winmode (#47)
    * Add WinPortal script
    
    * Fix final door prefab

---
+++ Assets/Scripts/WinPortal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPortal : MonoBehaviour
+{
+    private ChangeSceneWithButton sceneController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneController = FindObjectOfType<ChangeSceneWithButton>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            sceneController.LoadScene("Win");
+        }
+    }
+}
