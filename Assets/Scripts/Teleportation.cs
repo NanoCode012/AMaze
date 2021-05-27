ee1bf9446e322c812e927512a671ac33646b3ce8
NanoCode012 <kevinvong@rocketmail.com>
2021-05-27 20:45:03 +0700

    Update Teleportation.cs (#37)

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class Teleportation : MonoBehaviour
 {
-<<<<<<< Updated upstream
-
-=======
-    // Start is called before the first frame update
->>>>>>> Stashed changes
     public GameObject Destination;
 
     public void OnTriggerEnter(Collider other)
@@ -22,11 +17,7 @@ public class Teleportation : MonoBehaviour
 
     private IEnumerator Teleport(CharacterController controller)
     {
-<<<<<<< Updated upstream
-        yield return new WaitForSeconds(1);
-=======
         yield return new WaitForSeconds(1.5f);
->>>>>>> Stashed changes
         controller.enabled = false;
         controller.transform.position = Destination.transform.position;
         controller.enabled = true;
