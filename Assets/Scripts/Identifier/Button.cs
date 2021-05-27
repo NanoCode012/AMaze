f9c998b2ea049546f30b11537626ab02f9f11898
NanoCode012 <kevinvong@rocketmail.com>
2021-05-27 12:09:48 +0700

    Add button component

---
+++ Assets/Scripts/Identifier/Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Door target;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Use()
+    {
+        animator.SetTrigger("push");
+        target.Open();
+    }
+}
