dec80c5489c28f9e594fd712895bb30b202d4ff2
NanoCode012 <kevinvong@rocketmail.com>
2021-05-27 12:21:03 +0700

    Add finaldoor script

---
+++ Assets/Scripts/Identifier/FinalDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoor : MonoBehaviour
+{
+    private Animator animator;
+    private BoxCollider boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    public void Open()
+    {
+        animator.SetTrigger("open");
+        boxCollider.enabled = false;
+        // Play soundfx to opendoor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Open();
+    }
+}
