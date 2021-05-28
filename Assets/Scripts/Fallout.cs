462dfec09196cc5fb8eb09b3ec33a503e9555f8b
razermanth <78356885+razermanth@users.noreply.github.com>
2021-05-28 14:26:48 +0700

    Add fall death and misc changes (#48)
    * 1
    
    * Update SampleScene.unity
    
    Co-authored-by: NanoCode012 <kevinvong@rocketmail.com>

---
+++ Assets/Scripts/Fallout.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fallout : MonoBehaviour
+{
+    private ChangeSceneWithButton sceneController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneController = FindObjectOfType<ChangeSceneWithButton>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            sceneController.LoadScene("Lose");
+        }
+    }
+}
+
