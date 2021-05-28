13cdd96ab66adcdcbbb9d2e8e952592a587086c5
NanoCode012 <kevinvong@rocketmail.com>
2021-05-28 14:44:08 +0700

    Add playermanagerv1

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager playerManager;
+
+    void Awake()
+    {
+        if (playerManager) { Destroy(this.gameObject); }
+        else
+        {
+            playerManager = this;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            DontDestroyOnLoad(gameObject);
+
+            Setup();
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.name == "SampleScene")
+        {
+
+        }
+    }
+
+}
