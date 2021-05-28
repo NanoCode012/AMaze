486a43e3b9b69008303adfc056bbd68149100bf2
zombiedude91 <newnack2013@gmail.com>
2021-05-28 21:54:40 +0700

    add ghost AI

---
+++ Assets/Scenes/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+ 
+public class EnemyAI : MonoBehaviour {
+ 
+    public Transform player; 
+    private NavMeshAgent enemy; 
+ 
+ 
+    // Use this for initialization
+    void Start () {
+         
+ 
+        enemy = GetComponent<NavMeshAgent> ();
+ 
+    }
+     
+    // Update is called once per frame
+    void Update () {
+ 
+        //enemy.SetDestination (player.position); 
+        enemy.SetDestination(player.transform.position);
+         
+    }
+}
